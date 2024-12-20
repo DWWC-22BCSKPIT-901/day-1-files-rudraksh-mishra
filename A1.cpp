57865a35461d898ee14e073b76943020c0254466
rudraksh-mishra <t1839966@gmail.com>
2024-12-20 20:13:43 +0530

    Create A1.cpp
    File to solution of question 1 for day 1.

---
+++ A1.cpp
@@ -0,0 +1,11 @@
+// A1. Sum of natural numbers upto n.
+
+#include <iostream>
+
+int sum(int n) { return n * (n + 1) / 2; }
+
+int main() {
+	int n = 0;
+	std::cin >> n;
+	std::cout << "Sum of " << n << " : " << sum(n);
+}
